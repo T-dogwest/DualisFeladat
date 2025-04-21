0c6f5af3f111be38055317263d7a241e8b6c2ec3
csont_l7t39fj <csont_l7t39fj@TIBI>
2025-04-21 21:34:58 +0200

    rejtjelez osztály

--- DualisFeladat/Program.cs
+++ DualisFeladat/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Rejtjelez rejtjelez = new Rejtjelez();
+
+            //string uzenet = "helloworld";
+            //string kulcs = "abcdefgijkl";
+
+            //// Rejtjelezés
+            //string kodolt = rejtjelez.Rejtjelezes(uzenet, kulcs);
+            //Console.WriteLine("Eredeti üzenet: " + uzenet);
+            //Console.WriteLine("Kulcs: " + kulcs);
+            //Console.WriteLine("Rejtjelezett üzenet: " + kodolt);
+
+            //// Visszafejtés
+            //string visszafejtett = rejtjelez.Visszafejt(kodolt, kulcs);
+            //Console.WriteLine("Visszafejtett üzenet: " + visszafejtett);
+
+            //// Ellenőrzés
+            //if (visszafejtett == uzenet)
+            //{
+            //    Console.WriteLine("✅ A visszafejtés sikeres!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("❌ A visszafejtés hibás.");
+            //}
+           
         }
     }
 }
---
+++ DualisFeladat/Rejtjelez.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualisFeladat
+{
+    class Rejtjelez
+    {
+        //: a = 0, b = 1, ... z = 25, ' ' (szóköz) = 26
+        public int KarakterKod(char a)
+        {
+            return a == ' ' ? 26 : a - 'a';
+        }
+        public char KodKarakter(int kod)
+        {
+            return kod == 26 ? ' ' : (char)(kod + 'a');
+        }
+
+        public string Rejtjelezes(string uzenet, string kulcs)
+        {
+            if (kulcs.Length < uzenet.Length) throw new ArgumentException("a kulcsnak legalább akkorának kell lennie mint az üzenetnek");
+
+            char[] kodolt = new char[uzenet.Length];
+
+            for (int i = 0; i < uzenet.Length; i++)
+            {//a rejtjelezett üzenet n. karaktere az üzenet n. karakter kódja + kulcs n. karakter kódja    
+                int uzenetKod = KarakterKod(uzenet[i]);
+                int kulcsKod = KarakterKod(kulcs[i]);
+                int kodoltKod = uzenetKod + kulcsKod;
+                kodoltKod = kodoltKod > 26 ? kodoltKod % 27 : kodoltKod;
+                kodolt[i] = KodKarakter(kodoltKod);
+            }
+
+            return new string(kodolt);
+
+        }
+
+        public string Visszafejt(string kodolt, string kulcs)
+        {
+            if (kulcs.Length < kodolt.Length) throw new ArgumentException("a kulcsnak legalább akkorának kell lennie mint az üzenetnek");
+
+            char[] visszafejtett = new char[kodolt.Length];
+
+            for (int i = 0; i < kodolt.Length; i++)
+            {
+                int kodoltUzenetKod = KarakterKod(kodolt[i]);
+                int kulcsKod = KarakterKod(kulcs[i]);
+                int uzenetKod = (kodoltUzenetKod - kulcsKod + 27) % 27;
+                visszafejtett[i] = KodKarakter(uzenetKod);
+            }
+
+            return new string(visszafejtett);
+
+        }
+    }
+}
